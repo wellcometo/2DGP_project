1e79b7efc2c573dd363cbff67ee74d2177a520cc
gunho Lee <ghqwe123@naver.com>
2022-10-15 23:08:26 +0900

    game_framework, play_state, mygame added

---
+++ mygame.py
@@ -0,0 +1,6 @@
+import pico2d
+import game_framework
+
+pico2d.open_canvas()
+game_framework(play_state)
+pico2d.close_canvas()
--- project.py
+++
@@ -1,7 +0,0 @@
-import pico2d
-
-pico2d.open_canvas()
-events = pico2d.get_events()
-for event in events:
-    if event.type == pico2d.SDL_QUIT:
-        pico2d.close_canvas()
